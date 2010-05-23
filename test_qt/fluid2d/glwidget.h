088f608215aeb0ca711ef5ad0b38342ff96910b3
n.umetani <n.umetani@b674ca26-1ecb-11df-9ad9-9f9d9fb9d45e>
2010-05-23 21:54:07 +0000

    name change qt_test demos

---
+++ test_qt/fluid2d/glwidget.h
@@ -0,0 +1,43 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QGLWidget>
+
+#include "delfem/camera.h"
+#include "delfem/field.h"
+#include "delfem/field_world.h"
+#include "delfem/drawer_field.h"
+#include "delfem/eqnsys_fluid.h"
+
+class GLWidget : public QGLWidget
+{
+    Q_OBJECT
+
+public:
+    GLWidget(QWidget *parent = 0);
+    ~GLWidget();
+
+protected:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int width, int height);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+private slots:
+    void StepTime();
+
+public:
+    void SetNewProblem();
+private:
+    Com::View::CCamera camera;
+
+    Fem::Field::CFieldWorld world;
+    Fem::Field::View::CDrawerArrayField drawer_ary;
+    double cur_time;
+    double dt;
+    Fem::Eqn::CEqnSystem_Fluid2D fluid;
+    unsigned int id_base;
+};
+
+#endif
