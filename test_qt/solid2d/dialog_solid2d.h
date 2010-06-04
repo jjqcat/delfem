e62eb636a2c0dad71abc86e25286bfbcacbda473
n.umetani <n.umetani@b674ca26-1ecb-11df-9ad9-9f9d9fb9d45e>
2010-06-04 22:47:33 +0000

    Mat Prop Add in Qt Solid2d

---
+++ test_qt/solid2d/dialog_solid2d.h
@@ -0,0 +1,34 @@
+#ifndef DIALOG_SOLID2D_H
+#define DIALOG_SOLID2D_H
+
+#include <QDialog>
+
+#include "delfem/field.h"
+#include "delfem/field_world.h"
+#include "delfem/eqnsys_solid.h"
+
+namespace Ui {
+    class Dialog_Solid2D;
+}
+
+class Dialog_Solid2D : public QDialog {
+    Q_OBJECT
+public:
+    Dialog_Solid2D(const Fem::Field::CFieldWorld& world_input,
+                   Fem::Eqn::CEqnSystem_Solid2D& solid_input,
+                   QWidget *parent = 0);
+    ~Dialog_Solid2D();
+private slots:
+    void comboBox_currentIndexChenged(int);
+    void matPorpChanged();
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::Dialog_Solid2D *ui;
+
+    Fem::Eqn::CEqnSystem_Solid2D& solid;
+    const Fem::Field::CFieldWorld& world;
+};
+
+#endif // DIALOG_SOLID2D_H
